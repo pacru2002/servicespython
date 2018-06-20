af01ba5597b7e235e24c51dc7f89b2575b94ff11
pacru2002 <pacru_2002@yahoo.com>
2018-06-20 11:07:37 +0000

    initcode

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-import cv2
-
 app = Flask(__name__)
-
 empDB=[
  {
  'id':'101',
@@ -20,22 +17,10 @@ empDB=[
  'id':'301',
  'name':'Rajkumar P',
  'title':'Sr Software Engineer'
- },
-{
- 'id':'401',
- 'name':'Rajkumar P',
- 'title':'Sr Software Engineer'
- },
-{
- 'id':'501',
- 'name':'Rajkumar P',
- 'title':'Sr Software Engineer'
  }
-
  ]
 @app.route('/empdb/employee',methods=['GET'])
 def getAllEmp():
-    print("mytest")
     return jsonify({'emps':empDB})
 
 @app.route('/empdb/employee/<empId>',methods=['GET'])
@@ -70,28 +55,5 @@ def deleteEmp(empId):
     empDB.remove(em[0])
     return jsonify({'response':'Success'})
 
-@app.route('/empdb/fileProcess',methods=['POST'])
-def videoFileProcess():
-    #print("inside video file process")
-    if request.method == 'POST':
-        file = request.files['file']
-        #print(file.filename)
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-            #return redirect(request.url)
-        #if file and allowed_file(file.filename):
-        filename = file.filename
-        print("file name is "+filename)
-        blob = request.files['file'].read()
-        size = len(blob)
-        print(size)
-        print("after vc")
-        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print("after save")
-            #return redirect(url_for('uploaded_file',filename=filename))
-    return jsonify({'response':filename})
-   
 if __name__ == '__main__':
  app.run()
